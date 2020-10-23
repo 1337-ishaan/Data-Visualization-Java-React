d1e46677e3598df7c5e766f61f1e49d80055780e
1337-ishaan <1337.ishaan@gmail.com>
2020-10-23 09:44:56 +0000

    added options

--- src/App.js
+++ src/App.js
@@ -1,37 +1,10 @@
-import React from 'react';
-import CanvasJSReact from '../src/lib/canvasjs.react';
-//var CanvasJSReact = require('./canvasjs.react');
-// import logo from './logo.svg';
-import './App.css';
-import Home from '../src/modules/UI/Home'
-import Funnel from '../src/modules/UI/Funnel' 
-var CanvasJS = CanvasJSReact.CanvasJS;
-var CanvasJSChart = CanvasJSReact.CanvasJSChart;
-class App extends React.Component {	
-  render() {
-    const options = {
-      title: {
-        text: "Basic Column Chart in React"
-      },
-      data: [{				
-                type: "column",
-                dataPoints: [
-                    { label: "Apple",  y: 10  },
-                    { label: "Orange", y: 15  },
-                    { label: "Banana", y: 25  },
-                    { label: "Mango",  y: 30  },
-                    { label: "Grape",  y: 28  }
-                ]
-       }]
-   }
-		
-   return (
-      <div>
-        <CanvasJSChart options = {options}
-            /* onRef = {ref => this.chart = ref} */
-        />
-        <Funnel />
-      </div>
+import React, { Component } from 'react';
+import Template from './views/Template';
+
+class App extends Component {
+  render() {    
+    return (
+		<Template/>
     );
   }
 }
