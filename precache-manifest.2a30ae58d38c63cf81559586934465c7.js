63559fa43688b843bc3f3edfbf2813e461c2efe5
1337-ishaan <1337.ishaan@gmail.com>
2020-12-04 19:37:06 +0000

    Updates

---
+++ precache-manifest.2a30ae58d38c63cf81559586934465c7.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "daca0739ed8f51936f60ae2c07ef67a1",
+    "url": "/data-visualization-java-react/index.html"
+  },
+  {
+    "revision": "f5221dad8946dae76ed7",
+    "url": "/data-visualization-java-react/static/css/2.2a86faee.chunk.css"
+  },
+  {
+    "revision": "e2aa04f9bd7625a14753",
+    "url": "/data-visualization-java-react/static/css/main.26c159cd.chunk.css"
+  },
+  {
+    "revision": "f5221dad8946dae76ed7",
+    "url": "/data-visualization-java-react/static/js/2.4fee6efc.chunk.js"
+  },
+  {
+    "revision": "825b3f1924aa91c0b22224e02cce27f9",
+    "url": "/data-visualization-java-react/static/js/2.4fee6efc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e2aa04f9bd7625a14753",
+    "url": "/data-visualization-java-react/static/js/main.9a4a4ccd.chunk.js"
+  },
+  {
+    "revision": "462fda6c1001c3a5adb4",
+    "url": "/data-visualization-java-react/static/js/runtime-main.95b0695d.js"
+  }
+]);
