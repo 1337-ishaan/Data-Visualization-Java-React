2ff76e0024374ce7774dbc845af191aef98c675f
1337-ishaan <1337.ishaan@gmail.com>
2020-12-04 19:19:30 +0000

    deploy

--- src/views/dynamic charts/Dynamic Multi Series Chart.js
+++ src/views/dynamic charts/Dynamic Multi Series Chart.js
@@ -34,8 +34,14 @@ class DynamicMultiSeriesChart extends Component {
 			  y: yValue2
 			});
 		}
-		this.chart.options.data[0].legendText = " Bugatti Veyron - " + yValue1 + " km/h";
-		this.chart.options.data[1].legendText = " Lamborghini Aventador - " + yValue2 + " km/h";
+		
+		
+	// {options && 
+	// 	<>
+	// 	this.chart.options.data[0].legendText = " Bugatti Veyron - " + yValue1 + " km/h";
+	// 	this.chart.options.data[1].legendText = " Lamborghini Aventador - " + yValue2 + " km/h";
+	// 	</>
+	// }	
 		this.chart.render();
 	}
 	render() {
